f7419162f4318e4a6d517a0c3ef942c6e5851dfe
Jack <me@jackrbyrne.com>
2025-12-17 12:23:20 +0800

    Allow unfree fastmail-desktop from NixOS-25.11

--- flake.nix
+++ flake.nix
@@ -2,8 +2,11 @@
   description = "Jack's NixOS configuration";
 
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
-    nixpkgs.url = "nixpkgs/nixos-25.05";
+    # Newer channel for specific packages
+    nixpkgs-25_11.url = "github:NixOS/nixpkgs/nixos-25.11";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -20,11 +23,38 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, firefox-addons, plasma-manager, ... }: {
+  outputs = { self, nixpkgs, nixpkgs-25_11, home-manager, firefox-addons, plasma-manager, ... }:
+  let
+    system = "x86_64-linux";
+    lib = nixpkgs.lib;
+
+    # Allow ONLY this unfree package
+    allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+      "fastmail-desktop"
+    ];
+
+    # Import 25.11 with the unfree predicate applied
+    pkgs25_11 = import nixpkgs-25_11 {
+      inherit system;
+      config.allowUnfreePredicate = allowUnfreePredicate;
+    };
+
+    # Overlay: expose fastmail-desktop into your normal pkgs
+    fastmailOverlay = final: prev: {
+      fastmail-desktop = pkgs25_11.fastmail-desktop;
+    };
+  in
+  {
     nixosConfigurations.jack-pc = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+      inherit system;
 
       modules = [
+        # Enable overlay + (optional) also allow the same predicate on your main pkgs
+        ({ ... }: {
+          nixpkgs.overlays = [ fastmailOverlay ];
+          nixpkgs.config.allowUnfreePredicate = allowUnfreePredicate;
+        })
+
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
@@ -32,16 +62,16 @@
             useGlobalPkgs = true;
             useUserPackages = true;
 
-	    sharedModules = [
+            sharedModules = [
               plasma-manager.homeModules.plasma-manager
             ];
 
             users.jack = import ./home.nix;
             backupFileExtension = "backup";
 
-	    extraSpecialArgs = {
-	      inherit firefox-addons;
-	    };
+            extraSpecialArgs = {
+              inherit firefox-addons;
+            };
           };
         }
       ];
